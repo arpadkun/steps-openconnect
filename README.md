ad7791693462e4670f41c1db5a57c678b35a626f
Arpad Kun <arpadkun@arpadkun-mbp.lan>
2020-09-16 18:48:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bitrise Step to connect to Cisco Firepower VPN Terminator with Openconnect
+
+You can use this step to connect to a Cisco Firepower VPN Terminator with Openconnect.
+
+Requirements:
+   - installed and configured brew (if openconnect is not installed)
+
+Environment variables, should be set as secret in the bitrise dashboard!
+   - OPENCONNECT_VPN_TERMINATOR env var with IP address or host name
+   - OPENCONNECT_USER env var with VPN username
+   - OPENCONNECT_PASSWORD env var with VPN password
+   - OPENCONNECT_SERVERCERT env var with VPN fingerprint hash (like: pin-sha256:xyz1234....)
---
+++ step.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Requirements:
+#   - installed and configured brew (if openconnect is not installed)
+# Environment variables, should be set as secret in the bitrise dashboard!
+#   - OPENCONNECT_VPN_TERMINATOR env var with IP address or host name
+#   - OPENCONNECT_USER env var with VPN username
+#   - OPENCONNECT_PASSWORD env var with VPN password
+#   - OPENCONNECT_SERVERCERT env var with VPN fingerprint hash (like: pin-sha256:xyz1234....)
+
+
+
+set -e
+
+check_existence() {
+    if [ "x$1" = "x" ]; then
+        echo "ERROR: required \$$2 environment variable is missing or empty, exiting"
+        exit 1
+    fi
+}
+
+check_existence "$OPENCONNECT_VPN_TERMINATOR" "OPENCONNECT_VPN_TERMINATOR"
+check_existence "$OPENCONNECT_USER" "OPENCONNECT_USER"
+check_existence "$OPENCONNECT_PASSWORD" "OPENCONNECT_PASSWORD"
+check_existence "$OPENCONNECT_SERVERCERT" "OPENCONNECT_SERVERCERT"
+
+# Check if openconnect is installed
+openconnect --version &>/dev/null
+if [ $? -ne 0 ]; then
+    echo "Openconnect is not installed, calling brew to install it..."
+    brew install openconnect
+fi
+
+echo $OPENCONNECT_PASSWORD | sudo openconnect $OPENCONNECT_VPN_TERMINATOR --user=$OPENCONNECT_USER --passwd-on-stdin --servercert $OPENCONNECT_SERVERCERT
